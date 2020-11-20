b33e65f865373049ae0106c40c66985809171e56
internship@niconicotravel.com <shunkat@icloud.com>
2020-11-20 20:29:06 +0900

    test.jsをメソッド化して都市を入力したらその都市の気象情報を取ってくるようにする

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,23 +1,17 @@
-//htmlのul要素（id = 'messages'）を呼び出し
-var messageList = $('#messages');
+const getWeather= async (city) =>{
+  //openweathermap（天気予報API）に接続
+  let request = new XMLHttpRequest();
+  let cityName = city;
+  let owmApiKey = process.env.VUE_APP_WEATHER_API_KEY;
+  let ownURL = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&APPID="+ owmApiKey +""; //http://api.openweathermap.org/data/2.5/weather?q=tokyo&APPID=2146bc5634ee54009009695fae2a8d1c
 
-//openweathermap（天気予報API）に接続
-var request = new XMLHttpRequest();
-var cityName = "tokyo";
-var owmApiKey = "8c699e02bb0cc3f02814172a8cc758c7";
-var owmURL = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&APPID="+ owmApiKey +"";
+  const result = await fetch(ownURL)
+  .then((res)=>{
+    return res.json();
+  }).then((data)=>{
+    return data
+  });
+  return result
 
-request.open('GET', owmURL, true);
-//結果をjson型で受け取る
-request.responseType = 'json';
-
-request.onload = function () {
- var data = this.response;
- console.log(data);
- var messageElement = $("<il><p class='weather'>" + data["weather"][0]["main"] + "</p></il>");
- //HTMLに取得したデータを追加する
- messageList.append(messageElement);
- Logger.log(request)
-};
-
-request.send();
+}
+console.log(getWeather('tokyo'));
