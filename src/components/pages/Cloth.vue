dbf733c419edd9fc00551046aedbc2cc4fcc04f9
seiji <s.hoshino0517basket@gmail.com>
2020-11-26 18:35:49 +0900

    服装イラスト追加、変更

--- src/components/pages/Cloth.vue
+++ src/components/pages/Cloth.vue
@@ -20,13 +20,13 @@ computed:{
   clothesImage(){
   
    if(this.dressNumber==4){
-return "/img/t-shirt.png"
+return "/img/t-shirt.png"//半袖
     } else if (this.dressNumber==3) {
-      return "/img/y-shirt.png"
+      return "/img/y-shirt.png"//長袖
     } else if (this.dressNumber==2) {
-      return ""
+      return "/img/longt-shirt.png"//ジャケット
     } else if (this.dressNumber==1){
-      return "/img/jacket.png"
+      return "/img/jacket.png"//コート
     }
 return ""
   }
