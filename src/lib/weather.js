2921de350fb6d870985957b7f2068f74852dddac
長尾准誠 <junsei.nagao@gmail.com>
2020-11-26 14:13:11 +0900

    getTempatureを実装。lintがかかってわかりにくいですが、同じことをしてます。

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -1,16 +1,32 @@
-export const getWeather= async (lat,lon) =>{
+import { getLocation } from "@/lib/location.js";
+
+export const getWeather = async (lat, lon) => {
   //openweathermap（天気予報API）に接続
 
   let ownApiKey = process.env.VUE_APP_WEATHER_API_KEY;
-  let ownURL = "http://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon=" + lon +"&exclude=hourly,minutely&appid="+ ownApiKey +"";
+  let ownURL =
+    "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=hourly,minutely&appid=" +
+    ownApiKey +
+    "";
 
   const result = await fetch(ownURL)
-
-  .then((res)=>{
-    return res.json();
-  }).then((data)=>{
-    return data.daily[0].feels_like;
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data.daily[0].feels_like;
+    });
+  return result;
+};
+export const getTempature = () => {
+  const latAndLng = getLocation().then((result) => {
+    return result;
   });
-  return result
-
-}
+  const weather = getWeather(latAndLng.lat, latAndLng.lng).then((result) => {
+    return result;
+  });
+};
