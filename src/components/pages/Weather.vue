966cd590e8824a80ef4741efedd6e5d5507a4869
seiji <s.hoshino0517basket@gmail.com>
2020-12-01 11:50:01 +0900

    css構成作り

---
+++ src/components/pages/Weather.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container w-1/2">
+    <div class="w-full">
+      <div class="bg-gray-100 p-10 rounded-lg">
+        <h3 class="tracking-widest text-teal-500  font-medium title-font">
+          日付
+        </h3>
+        <h2 class="text-lg  font-medium title-font p-32">
+          イラスト
+        </h2>
+        <p class="leading-relaxed text-base">
+          気温など
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Weather",
+};
+</script>
+
+<style lang="scss" scoped></style>
