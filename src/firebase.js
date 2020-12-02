7c0d170e874d5a7a8b55ed413314480749fa76c9
internship@niconicotravel.com <shunkat@icloud.com>
2020-12-02 22:40:55 +0900

    基本

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi4s5LWqTohbnNBCRYHZyu6YIKfc8zLOA",
+  authDomain: "warmorchil.firebaseapp.com",
+  databaseURL: "https://warmorchil.firebaseio.com",
+  projectId: "warmorchil",
+  storageBucket: "warmorchil.appspot.com",
+  messagingSenderId: "86681130243",
+  appId: "1:86681130243:web:5a766f2cfb293e5a118536",
+  measurementId: "G-2VR80B5HKY"
+};
+
+
+firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import '@/assets/tailwind.css' //これ
+import "./firebase.js"//firebaseを使えるようにする
 Vue.config.productionTip = false;
 
 new Vue({
